b1f1a48fdd0c67eaff9eed9d3833f68d5a41c051
IsaacWines <isaacjwines@yahoo.com>
2026-02-11 13:38:19 -0600

    Code Dump from Private Working Repo

---
+++ Efield_Plot.py
@@ -0,0 +1,211 @@
+"""
+Calculate field values from electric potential, and plots the E_f vectors.
+Can handle multple csvs.
+
+Inputs:
+    Voltage Readings <- .CSV
+
+Outputs:
+    Electric Field X Component at Each Point -> .CSV file
+    Electric Field Y Component at Each Point -> .CSV file
+    Plot of Electric Field Vectors at each point -> .PNG Image File
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def csv_import(path: str, size: tuple) -> pd.DataFrame:
+    # Convert csv into dataframe
+    df = pd.read_csv(path, header=None, index_col=False)
+
+    # Normalize CSV Shape in return
+    return df.iloc[: size[0], : size[1]]
+
+
+def Ey_Calc(current_config: pd.DataFrame, size: tuple, step_size: float) -> tuple:
+    """
+    Function to return the calculated Ey Field from a config's csv file.
+
+    Inputs:
+        current_config <-- pandas DataFrame type of the config csv
+        size <-- tuple (number of rows , number of columns)
+        step_size <-- float distance between readings
+    Outputs:
+        Ey --> pandas DataFrame type of the Ey Field at each point from the config csv
+    """
+
+    Ey = {}
+    Ey_Formula = {}
+
+    for row in current_config.itertuples():
+        # Initialize list for each row
+        Ey[row[0]] = []
+        Ey_Formula[row[0]] = []
+
+        for v_reading in range(1, len(row[1:])):
+            Ey_point = -1 * ((row[v_reading + 1] - row[v_reading]) / step_size)
+            Ey[row[0]].append(Ey_point)
+            Ey_Formula[row[0]].append(f"{row[v_reading + 1]} - {row[v_reading]}")
+
+    return pd.DataFrame.from_dict(Ey).T.iloc[
+        : size[0], : size[1]
+    ], pd.DataFrame.from_dict(Ey_Formula).T.iloc[: size[0], : size[1]]
+
+
+def Ex_Calc(current_config: pd.DataFrame, size: tuple, step_size: float) -> tuple:
+    """
+    Function to return the calculated Ex Field from a config's csv file.
+
+    Inputs:
+        current_config <-- pandas DataFrame type of the config csv
+        size <-- tuple (number of rows , number of columns)
+        step_size <-- float distance between readings
+    Outputs:
+        Ex --> pandas DataFrame type of the Ex Field at each point from the config csv
+    """
+
+    Ex = {}
+    Ex_Formula = {}
+
+    for col in current_config.items():
+        # Initialize list for each row
+        Ex[col[0]] = []
+        Ex_Formula[col[0]] = []
+
+        for v_reading in range(len(col[1])):
+            try:
+                Ex_point = -1 * (
+                    (col[1][v_reading + 1] - col[1][v_reading]) / step_size
+                )
+
+                Ex[col[0]].append(Ex_point)
+                Ex_Formula[col[0]].append(
+                    f"{col[1][v_reading + 1]} - {col[1][v_reading]}"
+                )
+            except:
+                pass
+
+    return pd.DataFrame.from_dict(Ex).iloc[
+        : size[0], : size[1]
+    ], pd.DataFrame.from_dict(Ex_Formula).iloc[: size[0], : size[1]]
+
+
+def plot_data(x_data, y_data, magnitudes, filename):
+    """
+    Generates a quiver plot of the supplied data.
+    No return.
+    """
+
+    # Plot setup
+    plt.ion()
+    fig, ax = plt.subplots()
+
+    # Generate vector plot
+    pivot = "mid"
+    title = f"E Field Vector Plot of {filename[1]}"
+
+    q = ax.quiver(x_data, y_data, magnitudes, pivot=pivot)
+
+    ax.set_title(title)
+
+    # Add colorbar
+    cbar = plt.colorbar(q)
+    cbar.set_label("Vector magnitude", rotation=90)
+
+    # Display plot
+    plt.pause(0.1)
+    plt.draw()
+
+    # Save image
+    # filename = input("Enter a filename to save the plot: ")
+    print("Saving plot to {}".format(filename[0]))
+    fig.savefig(filename[0] + "_vector_plot.png", dpi=300)
+
+    # Cleanup
+    plt.clf()
+    plt.close()
+
+
+def main():
+    # Configs is a list of the information where the different readings are stored and how they should be outputted. Create a new config entry and folders for each different reading configurations.
+    """
+    Example Config Entry =
+    {
+        "path": "path/to/csv",                         <-- the path to the csv file of voltage readings
+        "name": "name_of_reading",                     <-- name of the csv file without the '.csv'
+        "Ex_Field": "path/to/E_field_x_component.csv", <-- this is a path for the script to output
+        "Ey_Field": "path/to/E_field_y_component.csv", <-- this is a path for the script to output
+        "folder": "config_a/",                         <-- folder where everything for specific configuration is stored
+        "nicename": "Config A",                        <-- name for the plot title, can be whatever you want
+        "dims": (int rows , int columns)               <-- size of the CSV file for this specific config
+    }
+    """
+    configs = [
+        {
+            "path": "path/to/csv",
+            "name": "name_of_reading",
+            "Ex_Field": "path/to/E_field_x_component.csv",
+            "Ey_Field": "path/to/E_field_y_component.csv",
+            "folder": "config_a/",
+            "nicename": "Config A",
+            "dims": (0, 0),
+        }
+    ]
+
+    # Adjust to the dimensions of the voltage readings CSV: (rows,columns)
+    size = (20, 28)
+
+    # adjust to the distance between readings, assume uniform distance in x and y direction.
+    step_size = 10.00
+
+    for current_config in configs:
+        print(current_config["name"])
+
+        ###----------Uncomment line below if each different config CSV has different dimensions----------###
+        # try:
+        #     size = current_config["dims"]
+        # except:
+        #     print("Invalid or Nonexistant dimensions from config\Attempting to Default to global size...")
+        ###----------------------------------------------------------------------------------------------###
+
+        # Convert current config into pd dataframe]
+        current_csv = csv_import(current_config["path"], size)
+
+        Ecalc_size = (size[0] - 1, size[1] - 1)
+        Ey, Ey_Formula = Ey_Calc(current_csv, Ecalc_size, step_size)
+        # print("Ey\n", Ey)
+        # print("Ey_Formula\n", Ey_Formula)
+
+        Ex, Ex_Formula = Ex_Calc(current_csv, Ecalc_size, step_size)
+        # print("Ex\n", Ex)
+        # print("Ex_Formula\n", Ex_Formula)
+
+        try:
+            Ex.to_csv(current_config["Ex_Field"], index=False, encoding="utf-8")
+            Ey.to_csv(current_config["Ey_Field"], index=False, encoding="utf-8")
+            print(
+                f"Successfully saved {current_config['name']} at {current_config['Ex_Field']} and {current_config['Ey_Field']}"
+            )
+        except Exception as e:
+            print("Unable to create component csv\nCaught error: ", e)
+
+        try:
+            x_data, y_data = Ex.to_numpy(), Ey.to_numpy()
+            magnitudes = np.sqrt(x_data**2 + y_data**2)
+            plot_data(
+                x_data,
+                y_data,
+                magnitudes,
+                (
+                    current_config["folder"] + current_config["name"],
+                    current_config["nicename"],
+                ),
+            )
+        except Exception as e:
+            print("Unable to plot: ", e)
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # E-Field-Vector-Plotter
-A simple python logic tool that takes in a csv of recorded voltage readings across a grid and outputs the E Field Vectors at each of those points, plotted with matplotlib. 
+## Isaac Wines
+
+> A simple python logic tool that takes in bulk CSV files of different recorded voltage readings across a grid and outputs the E Field Vectors at each of those points, plotted with matplotlib.
+
+## Setting up Environment
+
+1) Clone repo and set working directory to said folder
+2) If new setup:
+    - Set up your virtual environment, in terminal --> "python -m venv .venv" *or if using python3* "python3 -m venv .venv"
+3) If .venv is already made:
+    - in terminal --> "source .venv/bin/activate"
+4) Install dependancies, in terminal --> "pip install -r requirements.txt"
+
+## Using the Script
+
+> **Note:** This script assumes that each reading has a uniform distance between each point in the x and in the y direction. It also does not account for field in the z direction.
+> **Note:** The script for testing purposes can also output the following `Ex_formula` or `Ey_formula` variables, these are CSV files that instead of numbers in each point, outputs what two numbers are being subtracted to get the value for that specific Ex or Ey component. 
+
+1) Follow the comment in the code for the config setup, create a seperate folder for each different reading config, script will output everything pertaining to that specific config in its specific folder.
+2) Set the `step_size` variable to the distance between readings (or points). Script math based in **mm**.
+3) Set the `size` variable to the dimensions of the voltage reading CSV files. **Note:** This assumes all the reading CSV files are the same dimensions, if bulk calculating multiple different voltage reading CSV files, either set this value to the dimensions of the smallest CSV file or specify in the config each the dimensions of each different CSV file.
+4) If specifying each dimension, uncomment **Line 207** through **Line 210**.
+5) Run script, all plots and component CSV files will be saved to their respective folders.
+
+## Proper Folder Setup 
+
+> Before Running Script
+```txt
+E-Field-Vector-Plotter/
+├── .venv/
+├── config_example/
+│   └── config_example.csv
+├── requirements.txt
+├── Efield_Plot.py
+├── LICENSE
+└── README.md
+```
+
+> After Running Script
+```txt
+E-Field-Vector-Plotter/
+├── .venv/
+├── config_example/
+│   ├── config_example.csv
+│   ├── config_example_Ex.csv
+│   ├── config_example_Ey.csv
+│   └── config_example_vector_plot.png
+├── requirements.txt
+├── Efield_Plot.py
+├── LICENSE
+└── README.md
+```
+
+## Concept
+
+Imagine you have a grid where each value on said grid corresponds with a specific point on a table, with the top left box in the grid being the origin. Every box does not store positional data, but it does store the Electric Potential otherwise known as Voltage, at each point on this table. If you were to run a current through the table in different paths, then the Voltage at each point would change depending on the amount of current and the shape it takes through the table.
+In a 2 Dimensional space, the flow of these electrons can create an electric field in the X and in the Y dimensions. Through taking in the Voltage through equidistant measurements on our table, we can determine what the vectors of our electric field is. 
+
+The script takes in the Readings for point 0 then calculates the vector from point 0 to point 1, and so on until every point in the X and in the Y has been calculated to get the components of each E field vector at each point, and seperates them into two new relational grids. **This calculation will result in a 1 row loss and a 1 column loss** as it is doing vector calculations.
